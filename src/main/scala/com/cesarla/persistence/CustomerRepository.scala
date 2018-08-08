b1bf3fe472d9278e067060f03cec74e731aaab84
Cesar Luis Alvargonzalez <cesar.luis.alvargonzalez@zalando.de>
2018-08-08 20:18:28 +0200

    Adds auxiliar repositories

---
+++ src/main/scala/com/cesarla/persistence/CustomerRepository.scala
@@ -0,0 +1,22 @@
+package com.cesarla.persistence
+
+import com.cesarla.models.{Customer, CustomerId}
+
+import scala.collection.concurrent.TrieMap
+
+class CustomerRepository {
+  private[this] val customersById: TrieMap[CustomerId, Customer] = TrieMap.empty[CustomerId, Customer]
+  private[this] val customersByEmail: TrieMap[String, Customer] = TrieMap.empty[String, Customer]
+
+  def getCustomer(customerId: CustomerId): Option[Customer] = customersById.get(customerId)
+
+  def existsCustomer(customerId: CustomerId): Boolean = customersById.contains(customerId)
+
+  def existsCustomer(email: String): Boolean = customersByEmail.contains(email)
+
+  def setCustomer(customer: Customer): Unit = {
+    customersById.put(customer.id, customer)
+    customersByEmail.put(customer.email, customer)
+    ()
+  }
+}
---
+++ src/main/scala/com/cesarla/persistence/OperationRepository.scala
@@ -0,0 +1,17 @@
+package com.cesarla.persistence
+
+import com.cesarla.models.{Operation, OperationId}
+
+import scala.collection.concurrent.TrieMap
+
+class OperationRepository {
+  private[this] val map: TrieMap[OperationId, Operation] = TrieMap.empty[OperationId, Operation]
+
+  def getOperation(operationId: OperationId): Option[Operation] = map.get(operationId)
+
+  def setOperation(operation: Operation): Operation = {
+    map.put(operation.operationId, operation)
+    operation
+  }
+
+}
